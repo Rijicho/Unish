a80306234dbc5f5ac943a9d7af7bd3362ccdf807
Rijicho <rinacy.kurumiyubeshi@gmail.com>
2021-07-05 13:09:36 +0900

    add shpref command

---
+++ Runtime/BuiltInCommands/CmdShellConfig.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+
+namespace RUtil.Debug.Shell
+{
+    internal class CmdShellConfig : UnishCommandBase
+    {
+        public override string[] Ops { get; } =
+        {
+            "shpref",
+        };
+
+        public override (UnishCommandArgType type, string name, string defVal, string info)[] Params { get; } =
+        {
+            (UnishCommandArgType.String, "key", null, "変更したい項目 (bgcolor/prompt) "),
+            (UnishCommandArgType.String, "value", null, "値"),
+        };
+
+        public override string Usage(string op)
+        {
+            return "Shellの環境設定をします。";
+        }
+
+        protected override UniTask Run(IUnish shell, string op, Dictionary<string, UnishCommandArg> args,
+            Dictionary<string, UnishCommandArg> options)
+        {
+            if (string.IsNullOrEmpty(args["key"].s))
+            {
+                return shell.RunCommandAsync("man shpref");
+            }
+            switch (args["key"].s)
+            {
+                case "bgcolor":
+                {
+                    shell.View.BackgroundColor = shell.ColorParser.Parse(args["value"].s);
+                    return default;
+                }
+                case "prompt":
+                {
+                    shell.Prompt = args["value"].s;
+                    return default;
+                }
+                default:
+                    return shell.RunCommandAsync("man shpref");
+            }
+        }
+    }
+}
