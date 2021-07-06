3b327025f0cd2a4392d4d5296921a25ed0d8db09
Rijicho <rinacy.kurumiyubeshi@gmail.com>
2021-07-06 21:57:30 +0900

    add pwd command

---
+++ Runtime/BuiltInCommands/CmdPwd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace RUtil.Debug.Shell
+{
+    internal class CmdPwd : UnishCommandBase
+    {
+        public override string[] Ops { get; } =
+        {
+            "pwd",
+        };
+
+        public override (UnishCommandArgType type, string name, string defVal, string info)[] Params { get; } = { };
+
+        protected override UniTask Run(IUnish shell, string op, Dictionary<string, UnishCommandArg> args,
+            Dictionary<string, UnishCommandArg> options)
+        {
+            shell.SubmitTextIndented(Application.persistentDataPath);
+            return default;
+        }
+
+        public override string Usage(string op)
+        {
+            return "Show the full path of your current directory (= PersistentDataPath).";
+        }
+    }
+}
