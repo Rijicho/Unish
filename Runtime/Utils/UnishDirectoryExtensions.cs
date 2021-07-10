7730e96f9d29e2ac12f08684b90fa741dae2640c
Rijicho <rijicho.nl@gmail.com>
2021-07-10 13:12:38 +0900

    dll非依存化

--- Runtime/Utils/UnishDirectoryExtensions.cs
+++ Runtime/Utils/UnishDirectoryExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using static RUtil.Debug.Shell.PathConstants;
@@ -141,77 +140,7 @@ namespace RUtil.Debug.Shell
 
         public static bool IsRoot(this IUnishDirectorySystem directorySystem, string path)
         {
-            if (path == Root)
-            {
-                return true;
-            }
-
-            if (path == CurrentDir || path == Home)
-            {
-                return false;
-            }
-
-            if (path == ParentDir)
-            {
-                return string.IsNullOrEmpty(directorySystem.Current);
-            }
-
-            var slash = Root.AsSpan();
-            var tilde = Home.AsSpan();
-            var dot   = CurrentDir.AsSpan();
-            var dots  = ParentDir.AsSpan();
-
-            var span    = path.AsSpan();
-            var current = directorySystem.Current.AsSpan();
-            var k       = 0;
-            for (var i = 0; i < span.Length; i++)
-            {
-                var c = span[i];
-                if (c == Separator || i + 1 == span.Length)
-                {
-                    if (i + 1 == span.Length)
-                    {
-                        i++;
-                    }
-
-                    //  0    k  i
-                    //  aaaa/bbb/...
-                    var nextDirName = span.Slice(k, i - k);
-
-                    if (nextDirName.SequenceEqual(tilde))
-                    {
-                        current = ReadOnlySpan<char>.Empty;
-                    }
-                    else if (nextDirName.SequenceEqual(dot) || nextDirName.IsEmpty)
-                    {
-                        //same dir
-                    }
-                    else if (nextDirName.SequenceEqual(dots))
-                    {
-                        if (current.IsEmpty || current.SequenceEqual(slash))
-                        {
-                            current = slash;
-                        }
-                        else
-                        {
-                            current = current.Slice(0, current.LastIndexOf(Separator));
-                        }
-                    }
-                    else if (current.SequenceEqual(slash))
-                    {
-                        current = ReadOnlySpan<char>.Empty;
-                    }
-                    else
-                    {
-                        current = string.Concat(current.ToString(), Separator.ToString(), nextDirName.ToString()).AsSpan();
-                    }
-
-                    k = i + 1;
-                    i = k + 1;
-                }
-            }
-
-            return current.SequenceEqual(slash);
+            return ConvertToHomeRelativePath(directorySystem, path) == null;
         }
     }
 }
